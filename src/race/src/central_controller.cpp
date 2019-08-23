a5b0040bc2ced96752ab057e03ed86bed95082e1
JaeShinJun <jsstar1207@gmail.com>
2019-08-23 15:49:11 +0900

    commit for test

--- src/race/src/central_controller.cpp
+++ src/race/src/central_controller.cpp
@@ -27,6 +27,9 @@ struct Point {
 enum { BASE, };
 
 
+float path_arrived_threshold = 1.5;
+float yaw_refresh_threshold = 0.2;
+
 int mode = BASE;
 int current_path_index = 0;
 std::vector<Point> path;
@@ -47,6 +50,14 @@ float getAngle(std::vector<Point> v1, std::vector<Point> v2) {
     y1 = v1[1].y - v1[0].y;
     x2 = v2[1].x - v2[0].x;
     y2 = v2[1].y - v2[0].y;
+
+    float u1 = sqrt(x1*x1 + y1*y1);
+    float u2 = sqrt(x2*x2 + y2*y2);
+    x1 /= u1;
+    y1 /= u1;
+    x2 /= u2;
+    y2 /= u2;
+
     // std::cout << x1 << ' ' << y1 << ' ' << x2 << ' ' << y2 << std::endl;
     return asin((x1*y2-y1*x2)/((cal_distance(v1[0], v1[1])*(cal_distance(v2[0], v2[1]))))) * 180.0 / M_PI;
 
@@ -54,7 +65,7 @@ float getAngle(std::vector<Point> v1, std::vector<Point> v2) {
 
 void set_path() {
     std::string HOME = std::getenv("HOME") ? std::getenv("HOME") : ".";
-    std::ifstream infile(HOME+"/ISCC_2019/src/race/src/path4.txt");
+    std::ifstream infile(HOME+"/ISCC_2019/src/race/src/path5.txt");
     std::string line;
 
     float min_dis = 9999999;
@@ -84,7 +95,7 @@ void odom_callback(const nav_msgs::Odometry::ConstPtr& odom) {
     }
 
     if(mode == BASE && is_path_set) {
-        float steering, throttle=10;
+        float steering, throttle=6;
         std::vector<Point> v1, v2;
         v1.push_back(prev_position);
         // v1.push_back(Point(current_position.x+cos(yaw), current_position.y+sin(yaw)));
@@ -96,6 +107,8 @@ void odom_callback(const nav_msgs::Odometry::ConstPtr& odom) {
         race::drive_values drive_msg;
         drive_msg.throttle = (int)throttle;
         drive_msg.steering = (int)steering;
+        if(steering >= 28) steering = 28;
+	if(steering <= -28) steering = -28;
 
         // ROS_INFO("steering : %f", steering);
         std::cout << "steering : " << steering << std::endl;
@@ -103,9 +116,9 @@ void odom_callback(const nav_msgs::Odometry::ConstPtr& odom) {
         drive_msg_pub.publish(drive_msg);
     }
     std::cout << current_path_index << ' ' << cal_distance(current_position, prev_position) << std::endl;
-    if(cal_distance(current_position, path[current_path_index]) < 5.0) current_path_index++;
+    if(cal_distance(current_position, path[current_path_index]) < path_arrived_threshold) current_path_index++;
 
-    if(cal_distance(current_position, prev_position) > 2.5) {
+    if(cal_distance(current_position, prev_position) > yaw_refresh_threshold) {
         prev_position.x = current_position.x;
         prev_position.y = current_position.y;
     }
