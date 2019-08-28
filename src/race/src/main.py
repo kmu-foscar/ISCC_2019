d6703dc7de5db8c8d6195b440c4979163df535cd
ShineySun <ksp2246@naver.com>
2019-08-28 17:16:53 +0900

    modify for test

--- src/race/src/main.py
+++ src/race/src/main.py
@@ -68,8 +68,8 @@ def auto_drive(pid):
 
 def main():
     rospy.init_node('lane_detector_goni_node')
-    # cap = cv2.VideoCapture(0)
-    cap = cv2.VideoCapture("TEST14.avi")
+    cap = cv2.VideoCapture(0)
+    # cap = cv2.VideoCapture("TEST14.avi")
 
     while True:
         ret, img = cap.read()
@@ -86,23 +86,97 @@ def main():
       #out.write(img1)
       #out2.write(cv_image)
 
+def light_calc(frame):
+    arr = np.array(frame)
+
+    max_val = np.max(arr)
+    min_val = np.min(arr)
+
+    dst = (frame - min_val)*(255/(max_val - min_val))
+
+    # print('min_val : ' ,min_val)
+    # print('max_val : ', max_val )
+
+    # cv2.imshow("dst",dst)
+
+    return dst
 
 def process_image(frame):
 
-    # grayscle
-    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     # blur
     kernel_size = 5
-    blur_gray = cv2.GaussianBlur(gray,(kernel_size, kernel_size), 0)
+    blur = cv2.GaussianBlur(frame,(kernel_size, kernel_size), 0)
+
+    # img_bird = warper.warp(frame)
+
+    # cv2.imshow("img_bird",img_bird)
+
+    hsv = cv2.cvtColor(blur,cv2.COLOR_BGR2HSV)
+    h,s,v = cv2.split(hsv)
+
+    hls = cv2.cvtColor(blur,cv2.COLOR_BGR2HLS)
+    h,l,s = cv2.split(hls)
+
+    # cv2.imshow("h",h)
+    yellow_process_img = light_calc(s)
+    white_process_img = light_calc(l)
+
+    # cv2.imshow("enhance", yellow_process_img)
+    # cv2.imshow("white_mask",white_process_img)
+
+    # grayscle
+    # gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+
+
+    # # blur
+    # kernel_size = 5
+    # blur_gray = cv2.GaussianBlur(gray,(kernel_size, kernel_size), 0)
+    # blur_gray1 = cv2.GaussianBlur(yellow_process_img,(kernel_size, kernel_size), 0)
+    # blur_gray2 = cv2.GaussianBlur(white_process_img,(kernel_size, kernel_size), 0)
+
+    ret,binary_img = cv2.threshold(yellow_process_img, 70, 255, cv2.THRESH_BINARY)
+
+    # cv2.imshow("bi",binary_img)
+
+    ret1,binary_img1 = cv2.threshold(white_process_img, 170, 255, cv2.THRESH_BINARY)
+
+    # cv2.imshow("bi1",binary_img1)
+
+    img_mask = cv2.bitwise_or(binary_img,binary_img1)
+
+    # img_result = cv2.bitwise_and(binary_img,binary_img,mask = img_mask)
+
+    cv2.imshow("img_result",img_mask)
+
+
     # canny edge
     low_threshold = 60#60
     high_threshold = 70# 70
-    edges_img = cv2.Canny(np.uint8(blur_gray), low_threshold, high_threshold)
-    # warper
-    img = warper.warp(edges_img)
-    img1, x_location = slidewindow.slidewindow(img)
 
-    return img1, x_location
+    # edges_img = cv2.Canny(np.uint8(blur_gray), low_threshold, high_threshold)
+    # edges_img1 = cv2.Canny(np.uint8(binary_img), low_threshold, high_threshold)
+    # edges_img2 = cv2.Canny(np.uint8(binary_img1), low_threshold, high_threshold)
+    edges_img3 = cv2.Canny(np.uint8(img_mask), low_threshold, high_threshold)
+
+
+    # cv2.imshow("edges_img1",edges_img3)
+
+    # warper
+    # img = warper.warp(edges_img)
+    # bird = warper.warp(edges_img1)
+    # bird1 = warper.warp(edges_img2)
+    bird2 = warper.warp(edges_img3)
+
+    # cv2.imshow("bird",bird2)
+
+    # img1, x_location1 = slidewindow.slidewindow(img)
+    # img2, x_location2 = slidewindow.slidewindow(bird)
+    img3, x_location3 = slidewindow.slidewindow(bird2)
+
+    # print(x_location1)
+
+    return img3, x_location3
+
 
 if __name__ == '__main__':
     main()
